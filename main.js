bd14d97827933f94f5543e347721e9b8b29fb258
Arman Didandeh <arman.didandeh@gmail.com>
2018-05-23 18:15:56 -0400

    Signed-off-by: Arman Didandeh <arman.didandeh@gmail.com>

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
-d3.csv("sample/papers.csv", function(dataset) {
-console.log(dataset);
+d3.csv("papers.csv", function(dataset) {
+  var grid = d3.select("#grid")
+  .append("svg")
+  .attr("width","510px")
+  .attr("height","510px");
+
+  for (var row = 0; row < dataset.length; row++) {
+    row.x = row*10;
+    row.y = row*10;
+    row.width = 50;
+    row.height = 50;
+  }
+  
+  var row = grid.selectAll(".row")
+    .data(dataset)
+    .enter().append("g")
+    .attr("class", "row")
+    .selectAll(".square")
+    .data(function(d) {
+      console.log(d)
+      return d;
+    })
+    .enter().append("rect")
+    .attr("class","square")
+    .attr("x", function(d) {
+      console.log(d)
+      return d.x;
+    })
+    .attr("y", function(d) { return d.y; })
+    .attr("width", function(d) { return d.width; })
+    .attr("height", function(d) { return d.height; })
+    .on('click', function(d) {
+         d.click ++;
+         if ((d.click)%4 == 0 ) { d3.select(this).style("fill","#fff"); }
+       if ((d.click)%4 == 1 ) { d3.select(this).style("fill","#2C93E8"); }
+       if ((d.click)%4 == 2 ) { d3.select(this).style("fill","#F56C4E"); }
+       if ((d.click)%4 == 3 ) { d3.select(this).style("fill","#838690"); }
+      });
 });
 
 function gridData() {
@@ -15,13 +51,15 @@ function gridData() {
     data.push( new Array() );
     
     // iterate for cells/columns inside rows
-    for (var column = 0; column < 10; column++) {
+    for (var column = 0; column < 5; column++) {
       data[row].push({
         x: xpos,
         y: ypos,
         width: width,
         height: height,
-        click: click
+        click: click,
+        row_index: row,
+        column_index: column
       })
       // increment the x position. I.e. move it over by 50 (width variable)
       xpos += width;
@@ -36,32 +74,5 @@ function gridData() {
 
 var gridData = gridData();  
 // I like to log the data to the console for quick debugging
-console.log(gridData);
+// console.log(gridData[1]);
 
-var grid = d3.select("#grid")
-  .append("svg")
-  .attr("width","510px")
-  .attr("height","510px");
-  
-var row = grid.selectAll(".row")
-  .data(gridData)
-  .enter().append("g")
-  .attr("class", "row");
-  
-var column = row.selectAll(".square")
-  .data(function(d) { return d; })
-  .enter().append("rect")
-  .attr("class","square")
-  .attr("x", function(d) { return d.x; })
-  .attr("y", function(d) { return d.y; })
-  .attr("width", function(d) { return d.width; })
-  .attr("height", function(d) { return d.height; })
-  .style("fill", "#fff")
-  .style("stroke", "#222")
-  .on('click', function(d) {
-       d.click ++;
-       if ((d.click)%4 == 0 ) { d3.select(this).style("fill","#fff"); }
-     if ((d.click)%4 == 1 ) { d3.select(this).style("fill","#2C93E8"); }
-     if ((d.click)%4 == 2 ) { d3.select(this).style("fill","#F56C4E"); }
-     if ((d.click)%4 == 3 ) { d3.select(this).style("fill","#838690"); }
-    });
